f16e8e7f22e16fdd53e68e4ee0bcf3c963f81dd5
yupr <12003186+yupr@users.noreply.github.com>
2022-02-26 01:20:52 +0900

    format

--- pages/Connect/index.tsx
+++ pages/Connect/index.tsx
@@ -1,20 +1,17 @@
 import { instance } from '../../lib/axios';
 import React, { useEffect } from 'react';
 
-const Connect = () =>{
-
+const Connect = () => {
   useEffect(() => {
     const test = async (): Promise<void> => {
-      const result = await instance.get('https://api.github.com/users/hadley/orgs');
+      const result = await instance.get(
+        'https://api.github.com/users/hadley/orgs'
+      );
       console.log('result', result);
     };
-    void test()
+    void test();
   }, []);
 
-  return(
-    <div>check api response</div>
-  )
-
-
-}
+  return <div>check api response</div>;
+};
 export default Connect;
