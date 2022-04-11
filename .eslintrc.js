93543bdcda7375d944e3edcfc1bdaa3451d8ab27
yupr <12003186+yupr@users.noreply.github.com>
2022-04-12 00:23:03 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
 
-  //globalオブジェクトを宣言なしで参照可能にするのか知らせるための設定
+  // globalオブジェクトを宣言なしで参照可能にするのか知らせるための設定
   env: {
-    browser: true, //window, alertなどbrowserで動くグローバル変数をglobals設定に登録
+    browser: true, // window, alertなどbrowserで動くグローバル変数をglobals設定に登録
     es2021: true,
   },
 
+  // 構文解析: サポートしたいJSのオプションを指定(デフォルトではES5を使用)
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module', // use import/export
+    project: './tsconfig.json',
+  },
+
   // 複数のルールをまとめたconfiguration名を適用
-  // ここで指定可能なものは、sharable configuration オブジェクトと呼ぶ
   // sharable configuration のみを提供している パッケージには、`eslint-config-` というプレフィックスが付与されている
   extends: [
     'eslint:recommended',
@@ -18,16 +24,10 @@ module.exports = {
     'prettier',
   ],
 
-  // 使用するプラグインを使用。
+  // 使用するプラグインを使用
   // ルールを有効化するためにextendsで指定する必要がある
   // plugins: {
   //}.
-
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
 
   // extendsによる共有設定で大まかなルール設定を入れ、個別にルールを記載
   rules: {
