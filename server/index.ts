a52e41ec00deb269ba1fab66c13d2d61531bab46
yupr <12003186+yupr@users.noreply.github.com>
2022-06-08 01:28:54 +0900

    format

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response }  from 'express';
+import express, { Request, Response } from 'express';
 import next from 'next';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
