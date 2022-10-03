015e534ac0221ac16fdf519b4b9a2920fdfad365
yupr <12003186+yupr@users.noreply.github.com>
2022-10-04 00:08:29 +0900

    remove loader lib

--- src/pages/Design/index.tsx
+++ src/pages/Design/index.tsx
@@ -1,22 +1,10 @@
-import { useState } from 'react';
 import styles from './index.module.scss';
 import Button from '@/components/atoms/Button';
-import Loader from '@/components/atoms/Loader/Oval';
 
 const Design = () => {
-  const [isLoad, setIsLoad] = useState(true);
-
-  setTimeout(() => {
-    setIsLoad(false);
-  }, 1000);
-
   const login = () => {
     console.log('logined');
   };
-
-  if (isLoad) {
-    return <Loader isVisible={isLoad} />;
-  }
 
   return (
     <>
