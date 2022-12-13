3b13c64295955615c12df54ec4910db815b9d7aa
yupr <12003186+yupr@users.noreply.github.com>
2022-12-14 02:00:39 +0900

    fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
   root: true,
-
   // globalオブジェクトを変数の宣言なしでもエラーを吐かずに参照可能
   env: {
-    browser: true, // window, alertなどbrowserで動くグローバル変数をglobals設定に登録
+    browser: true, // window, alert など browser で動くグローバル変数を globals 設定に登録
     es2021: true,
   },
-
   parser: '@typescript-eslint/parser',
-
   // 構文解析: サポートしたいJSのオプションを指定(デフォルトではES5を使用)
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module', // use import/export
     project: './tsconfig.json',
   },
-
-  // 複数のルールをまとめたconfiguration名を適用
+  // 複数のルールをまとめた configuration 名を適用
   // sharable configuration のみを提供している パッケージには、`eslint-config-` というプレフィックスが付与されている
   extends: [
     'eslint:recommended',
@@ -24,10 +20,10 @@ module.exports = {
     'next', //create-next-appで標準に搭載されたルールセット
     'next/core-web-vitals', //create-next-appで標準に搭載されたより厳格なルールセット
     'prettier',
+    'plugin:storybook/recommended',
   ],
-
   // 使用するプラグインを使用
-  // ルールを有効化するためにextendsで指定する必要がある
+  // ルールを有効化するために extends で指定する必要がある
   // plugins: {
   //}.
 
