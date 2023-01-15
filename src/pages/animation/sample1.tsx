30dde21e09ca74387499bd1795a56cd8f025cc07
yupr <12003186+yupr@users.noreply.github.com>
2023-01-15 21:54:43 +0900

    add gsap

--- src/pages/animation/sample1.tsx
+++ src/pages/animation/sample1.tsx
@@ -1,7 +1,29 @@
+import { gsap } from 'gsap';
+import { useRef, useEffect } from 'react';
+import { css } from '@emotion/react';
+
 const Sample1 = () => {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(divRef.current, {
+      backgroundImage: 'linear-gradient(to left, #09C6F9, #045DE9)',
+      duration: 5,
+    });
+  }, [divRef]);
+
   return (
     <>
-      <h1>Gsap</h1>
+      <div
+        ref={divRef}
+        css={css({
+          background: 'linear-gradient(to left, #9f92ff, #ff7689);',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+        })}
+      >
+        <h1>5秒かけてグラデーションの色が変化</h1>
+      </div>
     </>
   );
 };
