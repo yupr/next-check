6bb569b5219a9a8a6d5f82f86146c82c82ca160c
yupr <12003186+yupr@users.noreply.github.com>
2023-02-07 02:25:06 +0900

    replace mutateAsync to mutate

--- src/pages/Connect/index.tsx
+++ src/pages/Connect/index.tsx
@@ -10,32 +10,30 @@ const Connect = () => {
   const fetchLogin = useLogin();
   const [loginMsg, setLoginMsg] = useState<string>('');
 
-  const login = async () => {
-    try {
-      await fetchLogin.mutateAsync(
-        { userName: 'carl', pass: 'password' },
-        {
-          onSuccess: (res) => {
-            if (res?.message) {
-              setLoginMsg(res.message);
-            }
-          },
-        }
-      );
-    } catch (err) {
-      if (err instanceof AxiosError) {
-        // エラーメッセージをトーストなどに表示
-        const message = err ? err?.message : 'server error';
-        console.log('message', message);
+  const login = () => {
+    fetchLogin.mutate(
+      { userName: 'carl', pass: 'password' },
+      {
+        onSuccess: (res) => {
+          if (res?.message) {
+            setLoginMsg(res.message);
+          }
+        },
+        onError: (err) => {
+          if (err instanceof AxiosError) {
+            const message = err?.message || 'server error';
+            console.error(message);
+          }
+        },
       }
-    }
+    );
   };
 
   if (isLoading) {
     return <span>Loading...</span>;
   }
 
-  if (isError && error) {
+  if (isError) {
     const statusCode = error.response?.status;
     return <Error statusCode={statusCode}></Error>;
   }
