0b6dccef1a4337a64ebca3becd49dcbee0fab904
yupr <12003186+yupr@users.noreply.github.com>
2023-03-27 22:22:34 +0900

    fix comment

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -8,30 +8,31 @@ dayjs.extend(timezone);
 
 dayjs.tz.setDefault('Asia/Tokyo');
 
-// utc
+// UTC
 export const getUtcTime = () => {
   const utcTime = dayjs().utc().format();
   return utcTime;
 };
 
+// 時間
 export const getTime = () => {
   const time = dayjs().tz().format();
   return time;
 };
 
-// 時間
+// 時間 (日本語表記)
 export const getTimeJa = () => {
   const time = dayjs().tz().format('YYYY年MM月DD日HH:mm');
   return time;
 };
 
-// 日付
+// 日付 (日本語表記)
 export const getDateJa = () => {
   const date = dayjs().tz().format('MM月DD日');
   return date;
 };
 
-// 時間 -> 日付
+// 時間 -> 日付 (日本語表記)
 export const getTimeToDateJa = (time: string) => {
   const date = dayjs(time).tz().format('MM月DD日');
   return date;
