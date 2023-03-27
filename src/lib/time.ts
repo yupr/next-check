cb24849f9af01ddce2c5b0a0fc6584fde370e817
yupr <12003186+yupr@users.noreply.github.com>
2023-03-27 22:20:23 +0900

    dayjsの設定ファイルを追加

---
+++ src/lib/time.ts
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault('Asia/Tokyo');
+
+// utc
+export const getUtcTime = () => {
+  const utcTime = dayjs().utc().format();
+  return utcTime;
+};
+
+export const getTime = () => {
+  const time = dayjs().tz().format();
+  return time;
+};
+
+// 時間
+export const getTimeJa = () => {
+  const time = dayjs().tz().format('YYYY年MM月DD日HH:mm');
+  return time;
+};
+
+// 日付
+export const getDateJa = () => {
+  const date = dayjs().tz().format('MM月DD日');
+  return date;
+};
+
+// 時間 -> 日付
+export const getTimeToDateJa = (time: string) => {
+  const date = dayjs(time).tz().format('MM月DD日');
+  return date;
+};
