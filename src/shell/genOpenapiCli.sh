b46ba308815ad9d7c2095392bc6bc27b760ee5b1
yupr <12003186+yupr@users.noreply.github.com>
2023-05-21 23:36:48 +0900

    openapi generatorの環境構築 (#18)
    * create swagger
    
    * openapi-generatorの環境構築とユーザー情報取得apiを元に生成
    
    * apiクライアント生成用のshellを追加
    
    * fix
    
    * 自動で指定のディレクトリにAPIクライアントを生成するshell scriptを実装
    
    * maxdepthを0に変更

---
+++ src/shell/genOpenapiCli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir_path=(src/api/docs/*)
+
+# 念の為、直下のフォルダのみを検出
+dirs=$(find "${dir_path[0]}" -maxdepth 0 -type d)
+
+for dir in $dirs; do
+  # 生成元
+  Gen_Origin_PATH="$dir/swagger.json"
+
+  # 生成先
+  Gen_Destination_PATH="src/api/cli/$(basename "$dir")"
+
+  # APIクライアントを指定のディレクトリに生成
+  ./node_modules/.bin/openapi-generator-cli generate -g typescript-axios -i "$Gen_Origin_PATH" -o "$Gen_Destination_PATH"
+done
