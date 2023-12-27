b64542bdaa348728552964f6e22537419d8d24d4
yupr <12003186+yupr@users.noreply.github.com>
2023-12-28 00:49:52 +0900

    fix build err & remove unused file

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,6 +1,7 @@
 import { useApi } from '@/hooks/common/useApi';
 import { User } from '@/api/cli/user';
 import { UserApiFactory } from '@/api/cli/user';
+import { isClient } from '@/utils';
 
 const getUser = async (): Promise<User> => {
   const res = await UserApiFactory().loginUser();
@@ -8,7 +9,7 @@ const getUser = async (): Promise<User> => {
 };
 
 const useUser = () => {
-  const isAuth = localStorage.getItem('isAuth');
+  const isAuth = isClient ? localStorage.getItem('isAuth') : false;
   return useApi(['user'], async () => getUser(), { enabled: !!isAuth });
 };
 
