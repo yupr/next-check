f8a816eab0310521faa5f223dc274a04b163bd7d
yupr <12003186+yupr@users.noreply.github.com>
2024-08-18 17:56:15 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,112 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '@/styles/Home.module.scss';
+import { Box, Container, Grid, Paper, Stack, Typography } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <Head>
-          <title>窓口</title>
-        </Head>
+    <Container maxWidth="xl">
+      <Stack direction={'row'}>
+        <Box height={500} width={1300} display="flex" alignItems="center">
+          <img
+            src="/img/pexels-printexstar.webp"
+            width="100%"
+            height="100%"
+            style={{ objectFit: 'cover' }}
+            alt="pexels-printexstar"
+          />
+        </Box>
+      </Stack>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>Link</h1>
+      <Stack direction={'row'} justifyContent={'center'}>
+        <Paper
+          elevation={5}
+          square={false}
+          sx={{
+            width: '97%',
+            marginTop: '-65px',
+            height: 500,
+            backgroundColor: 'rgba(255, 255, 255, 0.8)',
+            backdropFilter: 'saturate(200%) blur(30px)',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Grid
+            width={'80%'}
+            container
+            marginTop={'30px'}
+            spacing={3}
+            justifyContent={'center'}
+            marginX={'auto'}
+            textAlign={'center'}
+          >
+            <Grid xs={4}>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                color={'rgb(26, 115, 232)'}
+              >
+                70+
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h5">
+                Coded Elements
+              </Typography>
+              <Typography
+                variant="body2"
+                gutterBottom
+                color={'rgb(123, 128, 154)'}
+              >
+                From buttons, to inputs, navbars, alerts or cards, you are
+                covered
+              </Typography>
+            </Grid>
 
-          <div className={styles.grid}>
-            <Link href="/inspect/connect" className={styles.card}>
-              <h2>Connect &rarr;</h2>
-              <p>For check api connect</p>
-            </Link>
+            <Grid paddingX={'5px'} xs={4}>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                color={'rgb(26, 115, 232)'}
+              >
+                15+
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h5">
+                Design Blocks
+              </Typography>
+              <Typography
+                color={'rgb(123, 128, 154)'}
+                variant="body2"
+                gutterBottom
+              >
+                Mix the sections, change the colors and unleash your creativity
+              </Typography>
+            </Grid>
 
-            <Link href="/inspect/pixi" className={styles.card}>
-              <h2>Pixi</h2>
-              <p>render canvas</p>
-            </Link>
-
-            <div className={styles.card}>
-              <h2>Material UI</h2>
-
-              <Link href="/mui/paging">
-                <h3>Pagination</h3>
-              </Link>
-
-              <Link href="/mui/checkbox">
-                <h3>List</h3>
-              </Link>
-            </div>
-
-            <div className={styles.card}>
-              <h2>Animation</h2>
-
-              <Link href="/animation/sample1">
-                <h3>sample1</h3>
-              </Link>
-            </div>
-          </div>
-        </main>
-      </div>
-    </>
+            <Grid xs={4}>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                color={'rgb(26, 115, 232)'}
+              >
+                4
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h5">
+                Pages
+              </Typography>
+              <Typography
+                variant="body2"
+                gutterBottom
+                color={'rgb(123, 128, 154)'}
+              >
+                Save 3-4 weeks of work when you use our pre-made pages for your
+                website
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Stack>
+    </Container>
   );
 };
 
